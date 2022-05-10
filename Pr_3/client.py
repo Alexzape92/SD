f4181d0a4924229e50b42a30cbbea0125fdaccaa
Alejandro Guitarte Fernandez <alexguitarte@gmail.com>
2022-05-10 13:20:58 +0200

    TERMINADA LA P3 DE LOS WEBOS

--- Pr_3/client.py
+++ Pr_3/client.py
@@ -4,50 +4,118 @@ import requests
 
 headers = {'Content-Type': 'application/json'}  #Para indicarle al data que es un json
 
-user = input('Introduce tu nick: ')
-passw = input('Introduce tu contraseña: ')
+def main():
+    while True:
+        user = input("Introduce tu nombre de usuario: ")
+        passw = input("Introduce tu contraseña: ")
 
-saladict = {
-    "idSala": 21,
-    "capacidad": 50,
-    "recursos": ["proyector", "rotuladores", "altavoces"],
-    "user": user,
-    "passw": passw
-}
 
-salajson = json.dumps(saladict, indent = 4)
-if requests.post(url='http://localhost:8000/addRoom', data=salajson, headers=headers).text == 'Credenciales no válidas':
-    print('Credenciales no válidas')
-    exit()
+        print("\t1. Añadir sala")
+        print("\t2. Mostrar información de la sala")
+        print("\t3. Añadir reserva")
+        print("\t4. Listar reservas")
+        print("\t5. Eliminar reserva")
+        print("\t6. Exit")
+        print("")
 
-#userdict = {
-#    "user": user,
-#    "passw": passw
-#}
+        opcion = input("Selecciona la opción: ")
 
-#userjson = json.dumps(userdict, indent=4)
-#resp = requests.get(url='http://localhost:8000/showInformationRoom/21', data=userjson, headers=headers).text
+        if opcion == '1':
+            print("AÑADIR SALA----------------------------------------------------")
+            id = input("Introduce el id de la sala: ")
+            cap = input("Introduce la capacidad de la sala: ")
+            rss = list()
 
-#if resp == 'Credencialies no válidas':
-#    print(resp)
-#    exit()
+            aux = input("Introduce un recurso de [proyector, pizarra, rotuladores, altavoces, micrófono, puntero láser] ('fin' para terminar): ")
+            if aux != 'fin': rss.append(aux)
+            while aux != 'fin':
+                aux = input("Introduce un recurso de [proyector, pizarra, rotuladores, altavoces, micrófono, puntero láser] ('fin' para terminar): ")
+                if aux != 'fin': rss.append(aux)
+            
+            saladict = {
+                "roomId": id,
+                "capacity": cap,
+                "resources": rss,
+                "user": user,
+                "passw": passw
+            }
 
-#resp_str = json.loads(resp)
-#print(resp_str)
+            anadir_sala(json.dumps(saladict, indent=4))
+        
+        if opcion == '2':
+            print("MOSTRAR SALA--------------------------------------------------------")
+            id = input("Introduce el id de la sala: ")
 
-resdict = {
-    "user": user,
-    "passw": passw,
-    "idSala": 21,
-    "fecha": '6/5/2022',
-    "HoraInicio": "16:00",
-    "HoraFin": "18:30"
-}
+            userjson = json.dumps({"user":user, "passw":passw}, indent=4)
+            ver_sala(userjson, id)
+            
+        if opcion == '3':
+            print("AÑADIR RESERVA--------------------------------------------------------")
+            ids = input("Introduce el id de la sala a reservar: ")
+            fec = input("Introduce la fecha de la reserva")
+            ini = input("Introduce la hora de entrada")
+            fin = input("Introduce la hora de salida")
 
-resjson = json.dumps(resdict, indent=4)
-resp = requests.post(url='http://localhost:8000/addBooking', data=resjson, headers=headers).text
+            resvdict = {
+                "roomId": ids,
+                "date": fec,
+                "startTime": ini,
+                "endTime":fin,
+                "user": user,
+                "passw": passw
+            }
 
-if len(resp) > 0:
+            resvjson = json.dumps(resvdict, indent=4)
+            anadir_rsv(resvjson)
+        
+        if opcion == '4':
+            print("LISTAR RESERVAS-------------------------------------------------")
+            userjson = json.dumps({"user":user, "passw":passw}, indent=4)
+            dni = input("Introduce tu dni: ")   #Debemos preguntarle, la única alternativa sería añadir un endpoint que nos devuelva el dni dado el usuario y contraseña
+
+            ver_reservas(userjson, dni)
+        
+        if opcion == '5':
+            print("ELIMINAR RESERVA----------------------------------------------------------")
+            userjson = json.dumps({"user":user, "passw":passw}, indent=4)
+            id = input("Introduce el id de la reserva a eliminar: ")
+
+            borrar_reserva(userjson, id)
+
+        if opcion == '6':
+            print("Hasta la próxima!")
+            requests.post(url='http://localhost:8000/exit')
+            exit()
+
+def anadir_sala(salajson):
+    if requests.post(url='http://localhost:8000/addRoom', data=salajson, headers=headers).text == 'Credenciales no válidas':
+        print('Credenciales no válidas')
+        exit()
+
+def ver_sala(userjson, id):
+    resp = requests.get(url='http://localhost:8000/showInformationRoom/{}'.format(id), data=userjson, headers=headers).text
+
+    if resp == 'Credencialies no válidas':
+        print(resp)
+        exit()
+
+    resp_str = json.loads(resp)
+    print(resp_str)
+
+def anadir_rsv(resjson):
+    resp = requests.post(url='http://localhost:8000/addBooking', data=resjson, headers=headers).text
+
+    if len(resp) > 0:
+        if resp == 'Credenciales no válidas':
+            print(resp)
+            exit()
+
+        resp_str = json.loads(resp)
+        print(resp_str)
+
+def ver_reservas(userjson, dni):
+    resp = requests.get(url='http://localhost:8000/showBookings/{}'.format(dni), data=userjson, headers=headers).text
+
     if resp == 'Credenciales no válidas':
         print(resp)
         exit()
@@ -55,19 +123,9 @@ if len(resp) > 0:
     resp_str = json.loads(resp)
     print(resp_str)
 
-resdict = {
-    "user": user,
-    "passw": passw,
-    "idSala": 21,
-    "fecha": '6/5/2022',
-    "HoraInicio": "16:00",
-    "HoraFin": "18:30"
-}
+def borrar_reserva(userjson, id):
+    resp = requests.put(url='http://localhost:8000/deleteBooking/{}'.format(id), data=userjson, headers=headers).text
 
-resjson = json.dumps(resdict, indent=4)
-resp = requests.post(url='http://localhost:8000/addBooking', data=resjson, headers=headers).text
-
-if len(resp) > 0:
     if resp == 'Credenciales no válidas':
         print(resp)
         exit()
@@ -75,4 +133,5 @@ if len(resp) > 0:
     resp_str = json.loads(resp)
     print(resp_str)
 
-
+if __name__ == '__main__':
+    main()
--- Pr_3/server.py
+++ Pr_3/server.py
@@ -19,9 +19,9 @@ class Sala:
         self.recursos = recursos
     
     def __str__(self):
-        return "idSala = {}, capacidad = {}, recursos = {}".format(self.idSala, self.capacidad, self.recursos)
+        return "roomId = {}, capacity = {}, resources = {}".format(self.idSala, self.capacidad, self.recursos)
     def __repr__(self):
-        return "idSala = {}, capacidad = {}, recursos = {}".format(self.idSala, self.capacidad, self.recursos)
+        return "roomId = {}, capacity = {}, resources = {}".format(self.idSala, self.capacidad, self.recursos)
 
 class Reserva:
     def __init__(self, idReserva, idSala, DNI, fecha, ini, fin):
@@ -32,10 +32,10 @@ class Reserva:
         self.ini = ini
         self.fin = fin
     def __str__(self):
-        return "idReserva = % s, idSala = %s ,DNI = % s, fecha = % s, HoraInicio = % s, HoraFin = % s" % \
+        return "bookingId = % s, roomId = %s ,DNI = % s, date = % s, startTime = % s, endTime = % s" % \
         (self.idReserva, self.idSala, self.DNI, self.fecha, self.ini, self.fin)
     def __repr__(self):
-        return "idReserva = % s, idSala = %s ,DNI = % s, fecha = % s, HoraInicio = % s, HoraFin = % s" % \
+        return "bookingId = % s, roomId = %s ,DNI = % s, date = % s, startTime = % s, endTime = % s" % \
         (self.idReserva, self.idSala ,self.DNI, self.fecha, self.ini, self.fin)
 
 class Usuario:
@@ -44,9 +44,9 @@ class Usuario:
         self.nick = nick
         self.passwd = passwd
     def __str__(self):
-        return "DNI = % s, Username = % s, Password = % s" % (self.DNI, self.nick, self.passwd)
+        return "DNI = % s, UserName = % s, password = % s" % (self.DNI, self.nick, self.passwd)
     def __repr__(self):
-        return "DNI = % s, Username = % s, Password = % s" % (self.DNI, self.nick, self.passwd)
+        return "DNI = % s, UserName = % s, password = % s" % (self.DNI, self.nick, self.passwd)
 
 def jsonparser():
     file_name = "DB.json"
@@ -58,15 +58,15 @@ def jsonparser():
     lista_users = data["Usuarios"]
 
     for sala in lista_salas:
-        objsala = Sala(sala["idSala"], sala["capacidad"], sala["recursos"])
+        objsala = Sala(sala["roomId"], sala["capacity"], sala["resources"])
         salas.append(objsala)
 
     for reserva in lista_reservas:
-        objresv = Reserva(reserva["idResv"], reserva["idSala"], reserva["DNI"], reserva["fecha"], reserva["ini"], reserva["fin"])
+        objresv = Reserva(reserva["bookingId"], reserva["roomId"], reserva["DNI"], reserva["date"], reserva["startTime"], reserva["endTime"])
         reservas.append(objresv)
 
     for user in lista_users:
-        objuser = Usuario(user["DNI"], user["Nick"], user["passwd"])
+        objuser = Usuario(user["DNI"], user["userName"], user["password"])
         users.append(objuser)
 
 @post('/addRoom')
@@ -76,9 +76,9 @@ def add_room():
     if not checkCredenciales(datosSala['user'], datosSala['passw']):
         return "Credenciales no válidas"
     else:
-        id = datosSala['idSala']
-        cap = datosSala['capacidad']
-        rss = datosSala['recursos']
+        id = datosSala['roomId']
+        cap = datosSala['capacity']
+        rss = datosSala['resources']
 
         sala = Sala(id, cap, rss)
 
@@ -96,9 +96,9 @@ def show_room(id):
                 break
 
         auxdict = {
-            "idSala": sala.idSala,
-            "capacidad": sala.capacidad,
-            "recursos": sala.recursos
+            "roomId": sala.idSala,
+            "capacity": sala.capacidad,
+            "resources": sala.recursos
         }
         return json.dumps(auxdict, indent=4)
 
@@ -110,10 +110,10 @@ def add_resrv():
     if not checkCredenciales(datosResrv['user'], datosResrv['passw']):
         return "Credenciales no válidas"
     else:
-        ids = datosResrv['idSala']
-        fec = datosResrv['fecha']
-        horaini = datosResrv['HoraInicio']
-        horafin = datosResrv['HoraFin']
+        ids = datosResrv['roomId']
+        fec = datosResrv['date']
+        horaini = datosResrv['startTime']
+        horafin = datosResrv['endTime']
 
         if not salaOcupada(ids, fec, horaini, horafin):
             for u in users:
@@ -128,15 +128,81 @@ def add_resrv():
 
                 ]
             }
-            for r in reservas:
-                if r.fecha == fec and horaini > r.fin and horafin < r.ini:
-                    sala = salas.index(r.idSala)
-                    ocupDict["La sala que desea reservar está ocupada"].append(salas[sala].__str__())
+            for s in salas:
+                if not salaOcupada(s.idSala, fec, horaini, horafin):
+                    ocupDict["La sala que desea reservar está ocupada"].append(s.__str__())
             
             outjson = json.dumps(ocupDict)
             return outjson
 
+@get('/showBookings/<dni>')
+def mostrar_resvs(dni):
+    datosUser = request.json
 
+    if not checkCredenciales(datosUser['user'], datosUser['passw']):
+        return "Credenciales no válidas"
+
+    resdict = {
+        'reservas' : []
+    }
+    for r in reservas:
+        if r.DNI == dni:
+            rdict = {
+                "bookingId" : r.idReserva,
+                "roomId"    : r.idSala,
+                "DNI"       : r.DNI,
+                "date"     : r.fecha,
+                "startTime"   : r.ini,
+                "endTime"   : r.fin
+            }
+            resdict["reservas"].append(rdict)
+    
+    resjson = json.dumps(resdict, indent=4)
+    return resjson
+
+@put('/deleteBooking/<id>')
+def borrar_resv(id):
+    datosUser = request.json
+
+    if not checkCredenciales(datosUser['user'], datosUser['passw']):
+        return "Credenciales no válidas"
+    
+    for r in reservas:
+        if str(r.idReserva) == id:
+            reservas.remove(r)
+            return json.dumps("Reserva eliminada")
+    
+    return json.dumps("No existe el identificador de la reserva")
+
+@post('/exit')
+def salir():
+    file_name = "DB.json"
+    with open(file_name, "r") as f:
+        data = json.loads(f.read())
+
+    for s in salas:
+        sala = {
+            "roomId": s.idSala,
+            "capacity": s.capacidad,
+            "resources": s.recursos
+        }
+        data['Salas'].append(sala)
+
+    for r in reservas:
+        rsv = {
+            "bookingId": r.idReserva,
+            "roomId": r.idSala,
+            "DNI" : r.DNI,
+            "date": r.fecha,
+            "startTime": r.ini,
+            "endTime": r.fin
+        }
+        data['Reservas'].append(rsv)
+    
+    with open(file_name, "w") as f:
+        json.dump(data, f)
+    
+    exit(1)
 
             
 
@@ -152,7 +218,6 @@ def checkCredenciales(user, passw):
         if u.nick == user and u.passwd == passw:
             return True
     return False
-
 
 if __name__ == '__main__':
     jsonparser()
